907b3e3262a628e60687d139174b002427f19b2b
Shradhdha Parsana <shradhdha03@gmail.com>
2020-08-16 00:53:57 -0400

    app setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const quotes = require('./quotes.json');
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a list of quotes
+app.get('/api/quotes', (req, res) => {
+    res.json(quotes);
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port);
+console.log('App is listening on port ' + port);
